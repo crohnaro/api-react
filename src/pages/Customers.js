c9047dd3369dfb99dd6b21ac1f57d94a4500afdc
crohnaro <viniciosalfa@hotmail.com>
2023-02-28 11:27:39 -0300

    ajustando estilização do componente card

--- src/pages/Customers.js
+++ src/pages/Customers.js
@@ -1,39 +1,50 @@
 import { useState, useEffect } from "react";
-import axios from 'axios'
+import axios from "axios";
 import CustomersCard from "../components/CustomerCard";
+import { makeStyles } from "@material-ui/core/styles";
+
+import Grid from "@material-ui/core/Grid/";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  card: {
+    margin: theme.spacing(2),
+  },
+}))
 
 const Customers = () => {
-  const [customers, setCustomers] = useState([]) 
-  
-
-  console.log(customers)
-
+  const [customers, setCustomers] = useState([]);
+  const classes = useStyles()
 
   useEffect(() => {
-    axios.get('https://reqres.in/api/users')
-        .then(response => {
-            const { data } = response.data
-            setCustomers(data)
-        })
-  }, [])  
+    axios.get("https://reqres.in/api/users").then((response) => {
+      const { data } = response.data;
+      setCustomers(data);
+    });
+  }, []);
 
-  return(
+  return (
     <>
-        <h1>Customers</h1>
+      <h1>Customers</h1>
+      <Grid container>
         {
-            customers.map(item => (
-                <CustomersCard
+          customers.map((item) => (
+            <Grid item xs={12} md={4}>
+              <CustomersCard
                 name={item.first_name}
                 lastname={item.last_name}
                 email={item.email}
                 avatar={item.avatar}
-                />
-            ))
+                className={classes.card}
+              />
+            </Grid>
+          ))
         }
-        
+      </Grid>
     </>
-    
-  ) 
+  );
 };
 
 export default Customers;
