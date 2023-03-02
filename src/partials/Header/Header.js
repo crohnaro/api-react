a70a38a9482a7366776250a60151a59b67dbd904
crohnaro <crohnarodev@gmail.com>
2023-03-02 19:25:50 -0300

    adicionando menu drawer

--- src/partials/Header/Header.js
+++ src/partials/Header/Header.js
@@ -1,13 +1,22 @@
+import { useState } from 'react'
+
 import {
   AppBar,
   Toolbar,
   Typography,
   Button,
   IconButton,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
 } from "@material-ui/core";
 
 import { makeStyles } from "@material-ui/core/styles";
+
 import MenuIcon from "@material-ui/icons/Menu";
+import HomeIcon from "@material-ui/icons/Home"
 
 const useStyles = makeStyles(() => ({
   tittle: {
@@ -16,27 +25,49 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
+
+
 const Header = () => {
   const classes = useStyles()
 
+  const [menuOpen, setMenuOpen] = useState(false)
+
+  const handleToggleMenu = () => {
+    setMenuOpen(!menuOpen)
+  }
+
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <IconButton
-          size="large"
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" component="div" className={classes.tittle}>
-          News
-        </Typography>
-        <Button color="inherit">Login</Button>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={() => handleToggleMenu()}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" className={classes.tittle}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Drawer open={menuOpen} onClose={() => handleToggleMenu()}>
+        <ListItem>
+          <ListItemIcon><HomeIcon /></ListItemIcon>
+          <ListItemText>Home</ListItemText>
+        </ListItem>
+        <ListItem>
+          <ListItemIcon></ListItemIcon>
+          <ListItemText>Cadastro de Clientes</ListItemText>
+        </ListItem>
+      </Drawer>
+    </>
+
   );
 };
 
