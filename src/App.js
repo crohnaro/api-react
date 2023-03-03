962da5df130ae917817993b76bf0b71e5e87b2cb
crohnaro <crohnarodev@gmail.com>
2023-03-02 21:02:44 -0300

    ajustando estrutura do projeto para aceitar navegação usando history.push

--- src/App.js
+++ src/App.js
@@ -2,23 +2,29 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Customers from "./pages/Customers";
 import Home from "./pages/Home";
+
 import TemplateDefault from "./templates/Default/Default";
 import TemplatePage from "./templates/TemplatePage/Page"
 
 const App = () => {
   return (
-    <TemplateDefault>
-      <Router>
-        <Switch>
+
+    <Router>
+      <TemplateDefault>
+      <Switch>
+        
           <Route path="/customers">
             <TemplatePage title="Clientes" Component={Customers} />
           </Route>
           <Route path="/">
             <TemplatePage title="Home" Component={Home} />
           </Route>
-        </Switch>
-      </Router>
-    </TemplateDefault>
+        
+      </Switch>
+      </TemplateDefault>
+    </Router>
+
+
   );
 };
 
