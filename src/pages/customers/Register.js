9b45e302eb84cfc33c243b08bf631b7edb6433c0
crohnaro <viniciosalfa@hotmail.com>
2023-03-07 12:12:05 -0300

    iniciando estrutura de cadastros

---
+++ src/pages/customers/Register.js
@@ -0,0 +1,12 @@
+import TextField from "@material-ui/core/TextField";
+
+const Register = () => {
+  return (
+    <>
+      <TextField id="outlined-basic" label="Digite seu nome" />
+      <TextField id="outlined-basic" label="Digite seu cargo" />
+    </>
+  );
+};
+
+export default Register;
