ce8e1ec6b0f910c3050e99a33a164e565821c2ca
crohnaro <viniciosalfa@hotmail.com>
2023-03-19 12:34:37 -0300

    iniciando estrutura de login utilizando context-api

--- src/App.js
+++ src/App.js
@@ -4,26 +4,25 @@ import CustomersList from "./pages/customers/List";
 import CustomersRegister from "./pages/customers/Register";
 import Home from "./pages/Home";
 import CustomersEdit from "./pages/customers/Edit";
+import Login from "./pages/login/Login";
 
 import TemplateDefault from "./templates/Default/Default";
 import TemplatePage from "./templates/TemplatePage/Page";
+import TemplateClean from "./templates/Clean/Clean";
 
 const App = () => {
   return (
     <Router>
-      <TemplateDefault>
-        <Switch>
+      <Switch>
+        <Route path="/login">
+          <TemplateClean title="Acesso Restrito" Component={Login} />
+        </Route>
+        <TemplateDefault>
           <Route path="/customers/edit/:id">
-            <TemplatePage
-              title="Editar Clientes"
-              Component={CustomersEdit}
-            />
+            <TemplatePage title="Editar Clientes" Component={CustomersEdit} />
           </Route>
           <Route path="/customers/add">
-            <TemplatePage
-              title="Cadastro de Clientes"
-              Component={CustomersRegister}
-            />
+            <TemplatePage title="Cadastro de Clientes" Component={CustomersRegister} />
           </Route>
           <Route path="/customers">
             <TemplatePage title="Lista de Clientes" Component={CustomersList} />
@@ -31,8 +30,8 @@ const App = () => {
           <Route path="/">
             <TemplatePage title="Home" Component={Home} />
           </Route>
-        </Switch>
-      </TemplateDefault>
+        </TemplateDefault>
+      </Switch>
     </Router>
   );
 };
---
+++ src/pages/login/Login.js
@@ -0,0 +1,10 @@
+
+const Login = () => {
+    return (
+        <>
+        <h1>Oi</h1>
+        </>
+    )
+}
+
+export default Login
